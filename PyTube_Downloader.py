4e60472b3c7ce9c8209339b9ceadebe522d558e4
PikaThorachu <krullwarriorking@gmail.com>
2024-12-09 11:30:20 -0500

    Branch for coding the metadata updater method. This method should take the source file after download completes, then update the file's metadata.

--- PyTube_Downloader.py
+++ PyTube_Downloader.py
@@ -1,25 +1,25 @@
 # import packages
 from pytube import YouTube
-import eyed3
 import os
+import eyed3
 
 # List class Song objects for download
 src_files: list = []
 
 # Define destination path for downloads as constant
 # Make sure you don't use relative path
-DEST: str = "YOUR DEST GOES HERE"
+DEST: str = "YOUR DESTINATION FOLDER PATH GOES HERE"
 
 # Define the class "Song" to store metadata for each song
 class Song():
-    def __init__(self, url, title=None, artist=None, album=None, album_artist=None, track_number=None, file_name=None):
+    def __init__(self, url, title=None, artist=None, album=None, album_artist=None, track_number=None, file_path=None):
         self.url = url
         self.title = title
         self.artist = artist
         self.album = album
         self.album_artist = album_artist
         self.track_number = track_number
-        self.file_name = file_name
+        self.file_path = file_path
 
     # function to download the youtube file
     # Moved this function to be a method of class Song
@@ -36,11 +36,29 @@ class Song():
             new_file = base + '.mp3'
             # rename mp3 file
             os.rename(output, new_file)
-            src.file_name = new_file
+            src.file_path = new_file
 
         except:
             # Exception handling
             print(f"{src.url} failed to download")
+
+        # WAIT FOR THE FILE TO COMPLETE DOWNLOADING
+        # CHECK FOR THE FILE BEFORE RUNNING HELPER FUNCTION
+        # Need to code this section still
+
+        # Once the file has downloaded, then execute helper function
+        Song.metadata_updater(src)
+
+
+    def metadata_updater(src):
+        # load file
+        audiofile = eyed3.load(src.file_path)
+
+        # Update tags
+        audiofile.tag.artist = src.artist
+
+        # Save file
+        audiofile.tag.save()
     
 def main():
     # Input a list of Song objects to process
