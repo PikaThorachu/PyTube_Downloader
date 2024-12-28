44fa8a0a07c09c2c8f391dabe481aedd7ca5bc94
PikaThorachu <krullwarriorking@gmail.com>
2024-12-28 09:07:48 -0500

    Attempting to troubleshoot pytube failing to download

--- PyTube_Downloader.py
+++ PyTube_Downloader.py
@@ -1,68 +1,49 @@
 # import packages
-from pytube import YouTube
-import os
-import eyed3
+from pytube import YouTube, extract
 
 # List class Song objects for download
 src_files: list = []
 
 # Define destination path for downloads as constant
 # Make sure you don't use relative path
-DEST: str = "YOUR DESTINATION FOLDER PATH GOES HERE"
+DEST: str = "/home/pikathor/workspace/github.com/PikaThorachu/Real_Python/PyTube/Downloaded"
 
 # Define the class "Song" to store metadata for each song
 class Song():
-    def __init__(self, url, title=None, artist=None, album=None, album_artist=None, track_number=None, file_path=None):
+    def __init__(self, url: str, title: str=None, artist: str=None, album: str=None, album_artist: str=None, track_number: int=None, track_duration: int=None, file_path: str=None):
         self.url = url
         self.title = title
         self.artist = artist
         self.album = album
         self.album_artist = album_artist
         self.track_number = track_number
+        self.track_duration = track_duration
         self.file_path = file_path
 
     # function to download the youtube file
     # Moved this function to be a method of class Song
-    def download_yt(src, dest: str):
+    def download_yt(src, dest: str) -> None:
         
         # Create object using YouTube package
         try:
             yt = YouTube(src.url)
-            output = yt.streams.get_audio_only().download(output_path=dest)
-            # split file into base and extension 
-            # since ext not needed use _
-            base, _ = os.path.splitext(output)
-            # convert file to MP3
-            new_file = base + '.mp3'
-            # rename mp3 file
-            os.rename(output, new_file)
-            src.file_path = new_file
+                # Once figure out the next step (convert to MP3, set on_complete_callback to file conversion functoin)
+            yt.streams.get_audio_only().download(output_path=dest)
+            
 
         except:
             # Exception handling
             print(f"{src.url} failed to download")
 
-        # WAIT FOR THE FILE TO COMPLETE DOWNLOADING
-        # CHECK FOR THE FILE BEFORE RUNNING HELPER FUNCTION
-        # Need to code this section still
-
-        # Once the file has downloaded, then execute helper function
-        Song.metadata_updater(src)
-
-
-    def metadata_updater(src):
-        # load file
-        audiofile = eyed3.load(src.file_path)
-
-        # Update tags
-        audiofile.tag.artist = src.artist
-
-        # Save file
-        audiofile.tag.save()
+    def extract_youtube_metadata(src) -> dict:
+        youtube_metadata_dict = extract.metadata(src.url)
+        print(youtube_metadata_dict.values())
     
 def main():
     # Input a list of Song objects to process
-    src_files: list = []
+    src_files: list = [
+        Song("https://youtu.be/bcAEiSP-KUA")
+        ]
     for src in src_files:
         Song.download_yt(src, DEST)
     
